9672a9712d96a11b6af6e9ae88fcab769dae0b37
Halocline <mdglissmann@gmail.com>
2020-01-13 23:16:12 -0700

    adding objectives to challenge

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Heading, Paragraph } from 'grommet';
+import { Anchor, Heading, Paragraph, Text } from 'grommet';
 
 import { challenges } from '../data';
+import { Objective } from '../components';
 
 const Challenge = ({ challenge }) => {
-  const { name, location, promotion } = challenge;
+  const { name, location, objectives, promotion } = challenge;
 
   return (
     <>
       <Heading>{name}</Heading>
-      <Heading level={2}>{promotion.intro}</Heading>
+      <Anchor href={location.map}>{location.name}</Anchor>
+      <Text>{promotion.intro}</Text>
       <Paragraph>{promotion.description}</Paragraph>
+      {objectives && (
+        <>
+          <Heading level={2}>Objectives</Heading>
+          {objectives.map(objective => {
+            return <Objective key={objective.objective} />;
+          })}
+        </>
+      )}
     </>
   );
 };
---
+++ src/components/Objective.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Heading } from 'grommet';
+
+const Objective = () => {
+  return <Heading>Objective Name</Heading>;
+};
+
+export { Objective };
